80ea60f1be2504ff382857307ebc4a5471a8c298
wasif kareem <wasifkareem2762@gmail.com>
2026-01-22 16:59:29 +0530

    :fix:forms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 import tailwindcss from '@tailwindcss/vite';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://wervingindex.com',
+  prefetch: {
+    prefetchAll: true,
+    defaultStrategy: 'hover'
+  },
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [sitemap(), partytown()]
 });
